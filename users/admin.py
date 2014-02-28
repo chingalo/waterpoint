0fbc04320f1cf3bedcdf8b975ed760e6e456286a
joseph chingalo <profschingalo@gmail.com>
2014-02-28 23:22:38 +0300

    creates model and register it

---
+++ users/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from users.models import *
+
+
+admin.site.register( Administrator )
+admin.site.register( Engineer )
+admin.site.register( Chairperson )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+#model for all users of system includes COWOSO_Chairpeson, district_water_engineer, system administrator
+class Administrator(models.Model):
+	admin_id = models.CharField(max_length = 20)
+	admin_name = models.CharField(max_length = 200)
+	password = models.CharField(max_length = 200)
+	e_mail = models.EmailField(max_length = 200)
+	telphone_number =  models.CharField(max_length = 20)
+	address =  models.CharField(max_length = 200, blank = True)
+	sex = models.CharField(max_length = 6)
+	
+	def __unicode__(self): #for pyhton 3 : def __str__(self):
+		return self.admin_name
+
+class Engineer(models.Model):
+	engineer_id = models.CharField(max_length = 20)
+	engineer_name = models.CharField(max_length = 200)
+	password = models.CharField(max_length = 200)
+	e_mail = models.EmailField(max_length = 200)
+	telphone_number = models.CharField(max_length = 20)
+	address =  models.CharField(max_length = 200, blank = True)
+	sex = models.CharField(max_length = 6)
+	
+	def __unicode__(self): #for pyhton 3 : def __str__(self):
+		return self.engineer_name
+
+class Chairperson(models.Model):
+	cowoso_id = models.CharField(max_length = 20)
+	cowoso_chairperson_name = models.CharField(max_length = 200)
+	password = models.CharField(max_length = 200)
+	e_mail = models.EmailField(max_length = 200)
+	telphone_number =  models.CharField(max_length = 20)
+	address =  models.CharField(max_length = 200, blank = True)
+	physical_location_name = models.CharField(max_length = 200)
+	sex = models.CharField(max_length = 6)
+	
+	def __unicode__(self): #for pyhton 3 : def __str__(self):
+		return self.cowoso_chairperson_name
