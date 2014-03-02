7729a5bbe9bc39ebabb3923ea7c67ea233f2cda3
joseph chingalo <profschingalo@gmail.com>
2014-03-02 16:45:16 +0300

    solving urls conflicts during login

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from users import views
 
 urlpatterns = patterns('',
-   # url(r'^$', views.index, name='index'),
+    url(r'^$', views.authorize, name='authorize'),
    
 )
--- waterpoint/urls.py
+++ waterpoint/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls import patterns, include, url
-from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'waterpoint.views.home', name='home'),
@@ -16,14 +14,15 @@ urlpatterns = patterns('',
 
      url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
+    
    
 )+ staticfiles_urlpatterns()
 
 
 if settings.DEBUG:
     urlpatterns = patterns('',
-    url(r'^/', include('users.urls')),
-	url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    url(r'^users/', include('users.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT,
         'show_indexes': True}),
         url(r'',
