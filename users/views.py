12abc16edd7d8c1213cc1e5c65d712bcd2049615
joseph chingalo <profschingalo@gmail.com>
2014-03-02 16:48:20 +0300

    get and extract password and username from login form

--- users/views.py
+++ users/views.py
@@ -1,1 +1,19 @@
+from django.http import HttpResponseRedirect,HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404, render
+from django.core.context_processors import csrf
+from django.core.urlresolvers import reverse
 
+def authorize(request):
+	#taking username and password from login form
+	loginform = request.POST
+	
+	#extract username
+	usernamelist = loginform.getlist('username')
+	username = usernamelist[0]
+	
+	#extract password
+	passwordlist = loginform.getlist('password')
+	password = passwordlist[0]
+	
+	context={'choices':loginform,'username':username, 'password':password}
+	return render(request, 'test.html',context)
