a8300ce9e7ea4c862440387a7c8a16aa3aac745c
joseph chingalo <profschingalo@gmail.com>
2014-03-11 13:31:36 +0300

    solving urls issues

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', views.authorize, name='authorize'),
     url(r'^(?P<user_id>\d+)/createEngineer/$', views.createEngineer, name='createEngineer'),
     url(r'^(?P<user_id>\d+)/createChairperson/$', views.createChairperson, name='createChairperson'),
+    url(r'^(?P<user_id>\d+)/adminHome/$', views.adminHome, name='adminhome'),
+    
    
 )
