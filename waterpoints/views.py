f97d099bcab4c8a988aa83f3779dd93543c94510
joseph chingalo <profschingalo@gmail.com>
2014-03-19 21:55:19 +0300

    upadete creation form for water point

--- waterpoints/views.py
+++ waterpoints/views.py
@@ -3,21 +3,27 @@ from django.shortcuts import render_to_response, get_object_or_404, render
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
 from users.models import *
-from waterpoints.forms import WaterpointRegistration
+from waterpoints.forms import WaterpointRegistration, WaterpointPhotos
 from waterpoints.models import *
 
 #description of waterpoints
+		
 def waterpointDetail(request):
-	context = {'message': "ok"}
-	if request.POST:
-	 form = WaterpointRegistration(request.POST)
-	 if form.is_valid():
-	    form.save()	    
-         return render(request, 'test2.html',context)
-        else:
-		 form = WaterpointRegistration()
-		 args = {}
-		 args.update(csrf(request))
-		 args[ 'form' ] = form
-		 return render_to_response('test.html',args)	
+	#take form from template
+	form = WaterpointRegistration(request.POST)
+	message = ""
+	#checking if form is completed or not
+	if form.is_valid():
+		form.save()
+		message = "You  have successfull create new water point"
+		context = {'message':message}
+		return render (request, 'test2.html',context)
+	else:
+		message = "not"	
+		form = WaterpointRegistration()
+		context = {'form':form,'message':message}
+		return render(request,'waterpointdescription.html',context)
+		    
+		
+			
 
