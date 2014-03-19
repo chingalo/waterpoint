c36a165207cc4a732afc6daa0f87413d104066f6
joseph chingalo <profschingalo@gmail.com>
2014-03-19 23:09:11 +0300

    using user object in addmin pages

--- waterpoints/urls.py
+++ waterpoints/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import patterns, url
 from waterpoints import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.waterpointDetail, name = 'waterpointDetail'),
+    url(r'^(?P<user_id>\d+)/$', views.waterpointDetail, name = 'waterpointDetail'),
+    url(r'^imagesupload/$', views.waterpointPhotos, name = 'waterpointPhotos'),
+    
 )
--- waterpoints/views.py
+++ waterpoints/views.py
@@ -3,16 +3,20 @@ from django.shortcuts import render_to_response, get_object_or_404, render
 from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
 from users.models import *
-from waterpoints.forms import WaterpointRegistration, WaterpointPhotos
+from waterpoints.forms import WaterpointRegistration
 from waterpoints.models import *
 
 #description of waterpoints
 		
-def waterpointDetail(request):
+def waterpointDetail(request,user_id):
+	#get the current user
+	user = Engineer()
+	user = Engineer.objects.get(id = user_id )
+	
 	#take form from template
 	form = WaterpointRegistration(request.POST)
 	message = ""
-	#checking if form is completed or not
+	#checking if form is completed or not, and taking the action
 	if form.is_valid():
 		form.save()
 		message = "You  have successfull create new water point"
@@ -21,9 +25,23 @@ def waterpointDetail(request):
 	else:
 		message = "not"	
 		form = WaterpointRegistration()
-		context = {'form':form,'message':message}
+		context = {'form':form,'message':message, 'user':user}
 		return render(request,'waterpointdescription.html',context)
 		    
-		
+# images for water points
+def waterpointPhotos(request):
+	#retrieve all from imageupload form
+	form = request.POST
+	
+	#retrieve image form form	
+	imagelist = form.getlist('image') 
+	
+	
+	message = 'ok'
+	context = {'message':message,'result':imagelist}
+	return render (request, 'test2.html',context)
+	
+	
+			
 			
 
