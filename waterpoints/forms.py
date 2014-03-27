680566aa57312661274476b90d7970eceecc917a
joseph chingalo <profschingalo@gmail.com>
2014-03-27 16:04:14 +0300

    create form for upload images for a particular water point

--- waterpoints/forms.py
+++ waterpoints/forms.py
@@ -5,4 +5,7 @@ class WaterpointRegistration(forms.ModelForm):
 	class Meta:
 		model = Waterpoint
 		
-
+class Upload_waterpoint_photos(forms.ModelForm):
+	class Meta:
+		model = Waterpoint_photos
+		fields = ('photos','image_title','image')
