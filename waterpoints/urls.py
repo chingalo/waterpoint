aeb0a991a6582c99c0f969c3d803772d0a0a56b0
joseph chingalo <profschingalo@gmail.com>
2014-03-28 16:41:31 +0300

    finalize functionality for upload image for a given water point

--- waterpoints/urls.py
+++ waterpoints/urls.py
@@ -4,6 +4,6 @@ from waterpoints import views
 
 urlpatterns = patterns('',
     url(r'^(?P<user_id>\d+)/$', views.waterpointDetail, name = 'waterpointDetail'),
-    url(r'^imagesupload/$', views.waterpointPhotos, name = 'waterpointPhotos'),
+    url(r'^imagesupload/(?P<user_id>\d+)/$', views.waterpointPhotos, name = 'waterpointPhotos'),
     
 )
--- waterpoints/views.py
+++ waterpoints/views.py
@@ -29,41 +29,56 @@ def waterpointDetail(request,user_id):
 			context = {'message':message,'position':'engineer','user':user,'welcome_info':welcome_info}
 			return render (request, 'engineer.html',context)
 		else:
-			message = "Please fill all information below, for successfull create ner water point"	
+			warning = "Please fill all information below, for successfull create ner water point"	
 			form = WaterpointRegistration()
-			context = {'form':form,'message':message,'position':'engineer', 'user':user,'welcome_info':welcome_info}
+			context = {'form':form,'warning':warning,'position':'engineer', 'user':user,'welcome_info':welcome_info}
 			return render(request,'waterpointdescription.html',context)
 		    
 # images for water points
-def waterpointPhotos(request):	
-	#checking if request method is POST
-	if request.method == 'POST':
+def waterpointPhotos(request, user_id):
+	#get the current user
+	user = Engineer()
+	user = Engineer.objects.get(id = user_id )
+	welcome_info = "Welcome"
+	
+	if user.login_status == 'log_out':
+		message = 'Sorry! Currently you are not log in into this System.'
+		context = {'message':message}
+		return render(request, 'notlogin.html', context)
+	else:		
+						
+		#checking if request method is POST
+		if request.method == 'POST':
+					
+		#taking bounded form
+			form = form = Upload_waterpoint_photos(request.POST, request.FILES)
 			
-	#taking bounded form
-		form = form = Upload_waterpoint_photos(request.POST, request.FILES)
-		if form.is_valid():
-			#save the form and return successfull upload message
-			message = 'ok'
-			context = {'message':message}
-			form.save()
-			return render(request,'test.html',context)
-
-	#return new form if request method is not POST
-	else:
-		message = 'not yet'
-		form = Upload_waterpoint_photos()
-		context = {'message':message,'form':form}
-		return render(request,'test2.html',context)
-		
+			if form.is_valid():
+				#save the form and return successfull upload message if the form is completely filled
+				message = 'successfull upload'
+				context = {'message':message,'welcome_info':welcome_info,'position':'engineer', 'user':user,}
+				form.save()
+				return render(request,'engineer.html',context)	
+							
+			else:
+				#return new form to user if any field is empty
+				warning = 'Please fill all fileds in the for for successfull uploading of the image'
+				form = Upload_waterpoint_photos()
+				context = {'form':form,'warning':warning,'position':'engineer', 'user':user,'welcome_info':welcome_info}
+				return render(request,'waterpointphotos.html',context)
+				
+		#return new form if request method is not POST
+		else:
+			message = 'File all field below, to upload the image'
+			form = Upload_waterpoint_photos()
+			context = {'message':message,'form':form, 'position':'engineer', 'user':user,'welcome_info':welcome_info}
+			return render(request,'waterpointphotos.html',context)		
+			
 		
 		
 		
 	
-	#taking the form request
-	#form = Upload_waterpoint_photos(request.POST, request.FILES)
-	#message = form.is_valid()
-	#context = {'message':message}
-	#return render (request,'test.html',context)
+	
 	
 	
 			
