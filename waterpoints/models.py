b0acc3ed54105841592c74699daf2df4edc06bec
joseph chingalo <profschingalo@gmail.com>
2014-04-02 22:44:29 +0300

    update model ready for phase 2 of project

--- waterpoints/models.py
+++ waterpoints/models.py
@@ -1,9 +1,21 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 #model for static and dyanamic information 
 class Waterpoint(models.Model):
 	code_number = models.CharField(max_length = 20)
-	water_point_name = models.CharField(max_length = 200)
+	name = models.CharField(max_length = 200)
+	source_name = models.CharField(max_length = 200)
+	#water supply connection type
+	supply_connection_category = (
+	('waterpoint','waterpoint'),
+	('domestic','domestic'),
+	('business','business'),
+	('institutional','institutional'),
+	('industrial','industrial'),
+	)	
+	supply_connection = models.CharField(max_length=200, choices = supply_connection_category , default = 'waterpoint')
 	district = models.CharField(max_length = 200, blank = True)
 	region = models.CharField(max_length = 200, blank = True)
 	physical_location_name = models.CharField(max_length = 200)
@@ -11,18 +23,11 @@ class Waterpoint(models.Model):
 	longitude = models.CharField(max_length = 200 , blank = True)
 	fund = models.CharField(max_length = 200)
 	sponsor = models.CharField(max_length = 200)
-	
-	#choices for water point status
-	status_choices=(
-	('functioning','fuctioning'),
-	('non-functioning','non-functioning'),
-	('need to repair','need to repair'),
-	)
-	status = models.CharField(max_length=200, choices = status_choices , default = 'functioning')
-	
+		
 	def __unicode__(self): #for pyhton 3 : def __str__(self):
 		return self.water_point_name
-
+		
+#model for photos upload
 class Waterpoint_photos(models.Model):
 	photos = models.ForeignKey('Waterpoint', on_delete=models.CASCADE)
 	image = models.ImageField(upload_to = "waterpoint" )
@@ -31,3 +36,12 @@ class Waterpoint_photos(models.Model):
 	def __unicode__(self): #for pyhton 3 : def __str__(self):
 		return self.image_title
 
+#model for status of a given water point		
+class Waterpoint_status(models.Model):
+	waterpoint = models.ForeignKey('Waterpoint', on_delete=models.CASCADE)
+	status = models.CharField(max_length = 200)
+	status_date_update = models.DateTimeField(default=timezone.now)
+	
+	def __unicode__(self): #for pyhton 3 : def __str__(self):
+		return self.status
+	
