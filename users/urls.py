8775ce7ab34bf062b842fa9f1f343899357c4597
joseph chingalo <profschingalo@gmail.com>
2014-04-09 00:05:54 +0300

    add solve url for report part in district water engineer

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^(?P<user_id>\d+)/adminHome/$', views.adminHome, name='adminhome'),
     url(r'^(?P<user_id>\d+)/enginerHome/$', views.enginerHome, name='engineerhome'),
     url(r'^(?P<user_id>\d+)/(?P<user_e_mail>\S+)/(?P<user_password>\S+)/(?P<user_position>\S+)/logout/$', views.log_out, name='log_out'),
-    
+    url(r'^reportSummary/(?P<user_id>\d+)/$', views.report_summary_engineer, name='report_summary_engineer'),
+    url(r'^cowsoSummary/(?P<user_id>\d+)/$', views.cowso_summary_engineer, name='cowso_summary_engineer'),
    
 )
