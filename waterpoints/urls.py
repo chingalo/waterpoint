abdd99e952d9c08e9716d4db710f48c2e388bfaf
joseph chingalo <profschingalo@gmail.com>
2014-04-11 22:18:04 +0300

    add urls for silving viewing single waterconnection details

--- waterpoints/urls.py
+++ waterpoints/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     url(r'^(?P<user_id>\d+)/(?P<user_location>\S+)/$', views.update_waterpoint, name = 'update_waterpoint'),
     url(r'^waterConnectionSummaryEngineer/(?P<user_id>\d+)/$', views.water_connection_summary_engineer, name='water_connection_summary_engineer'),
     url(r'^waterConnectionSummaryAdmin/(?P<user_id>\d+)/$', views.water_connection_summary_admin, name='water_connection_summary_admin'),
-    
+    url(r'^waterConnectionDetailsEngineer/(?P<user_id>\d+)/(?P<water_connection_id>\d+)/$', views.water_connection_DetailsFromEngineer, name='water_connection_details_from_engineer'),
 )
